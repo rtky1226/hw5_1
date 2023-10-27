90b2ecc12d020e3bb8e74cd75edfb03c3a8476a4
Kaiyi Tan <kt10.iu.edu>
2023-10-26 20:07:30 -0400

    commit 4

--- benchmark.R
+++ benchmark.R
@@ -12,6 +12,7 @@ z = seq(-1, 1, length.out = 100)
 # Benchmarking
 results <- microbenchmark(
   llr_result = llr(omega = 2, x = x, y = y, z = z),
+  llr_new_result = llr_new(omega = 2, x = x, y = y, z = z),
   times = 1000 # number of times to run the expression for the benchmark
 )
 
--- llr_function.R
+++ llr_function.R
@@ -22,3 +22,30 @@ make_predictor_matrix <- function(x){
   X = cbind(1,x)
   return(X)
 }
+
+
+make_weight_matrix_new <- function(z,x,omega){
+  r = abs(x - z) / omega
+  W_values = ifelse(abs(r) < 1, (1 - abs(r)^3)^3, 0)
+  Wz = diag(W_values)
+  return(W_values)
+}
+
+
+compute_f_hat_new=function(z,x,y,omega){
+  Wz=make_weight_matrix_new(z,x,omega) 
+  X=make_predictor_matrix(x) 
+  f_hat=c(1,z)%*%solve(t(X)%*%(Wz*X))%*%t(X)%*%(Wz*y)
+  
+  
+  return(f_hat) 
+}
+
+llr_new=function(x,y,z,omega){
+  fits=sapply(z,compute_f_hat_new,x,y,omega) 
+  return(fits) 
+}
+
+
+
+
