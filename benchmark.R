87fe70e8c5bcaee3a220eaf670bdcefbae2acc3e
Kaiyi Tan <kt10.iu.edu>
2023-10-26 22:04:28 -0400

    final commit for main

--- benchmark.R
+++ benchmark.R
@@ -3,7 +3,7 @@ source("llr_function.R")
 
 set.seed(123)
 n = 15
-## a very simple regression model
+
 x = rnorm(n)
 y = rnorm(x + rnorm(n))
 z = seq(-1, 1, length.out = 100)
@@ -12,7 +12,7 @@ z = seq(-1, 1, length.out = 100)
 # Benchmarking
 results <- microbenchmark(
   llr_result = llr(omega = 2, x = x, y = y, z = z),
-  times = 1000 # number of times to run the expression for the benchmark
+  times = 1000
 )
 
 print(results)
