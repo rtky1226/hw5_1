e37fdd1d12c901487caa06815f051f26a76fa12e
Kaiyi Tan <kt10.iu.edu>
2023-10-26 22:03:09 -0400

    final commit for speed-test-2

--- benchmark.R
+++ benchmark.R
@@ -4,7 +4,6 @@ source("llr_function.R")
 
 set.seed(123)
 n = 15
-## a very simple regression model
 x = rnorm(n)
 y = rnorm(x + rnorm(n))
 z = seq(-1, 1, length.out = 100)
@@ -14,7 +13,7 @@ z = seq(-1, 1, length.out = 100)
 results <- microbenchmark(
   llr_result = llr(omega = 2, x = x, y = y, z = z),
   llr_new_result = llr_new(omega = 2, x = x, y = y, z = z),
-  times = 1000 # number of times to run the expression for the benchmark
+  times = 1000
 )
 
 print(results)
